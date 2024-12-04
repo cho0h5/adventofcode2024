286ce5cacf906ff5f5610314da75a05d7ae67131
Youngho Cho <dominoyh5@gmail.com>
2024-12-04 15:31:05 +0900

    day2-2: start

--- day2-2/src/main.rs
+++ day2-2/src/main.rs
@@ -1,9 +1,14 @@
 use std::fs::read_to_string;
 
 fn is_increasing(numbers: &Vec<i32>) -> bool {
+    fn is_valid(gap: i32) -> bool {
+        1 <= gap && gap <= 3
+    }
+
+    let mut count = 0;
+
     for i in 1..numbers.len() {
-        let gap = numbers[i] - numbers[i - 1];
-        if gap <= 0 || gap >= 4{
+        if !is_valid(numbers[i] - numbers[i - 1]) {
             return false;
         }
     }
@@ -11,9 +16,14 @@ fn is_increasing(numbers: &Vec<i32>) -> bool {
 }
 
 fn is_decreasing(numbers: &Vec<i32>) -> bool {
+    fn is_valid(gap: i32) -> bool {
+        -3 <= gap && gap <= -1
+    }
+
+    let mut count = 0;
+
     for i in 1..numbers.len() {
-        let gap = numbers[i] - numbers[i - 1];
-        if gap >= 0 || gap <= -4 {
+        if !is_valid(numbers[i] - numbers[i - 1]) {
             return false;
         }
     }
