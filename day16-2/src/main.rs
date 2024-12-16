da4b2e5b051710f74fd5665476100caedf13091e
Youngho Cho <dominoyh5@gmail.com>
2024-12-16 16:48:48 +0900

    day16-2: rewrite code but not works

--- day16-2/src/main.rs
+++ day16-2/src/main.rs
@@ -39,7 +39,6 @@ struct State {
     x: i32,
     y: i32,
     dir: i32,
-    cost2: i32,
 }
 
 impl Ord for State {
@@ -54,26 +53,22 @@ impl PartialOrd for State {
     }
 }
 
-fn print_vst3(vst2: &[Vec<bool>]) {
-    for line in vst2 {
+fn print_vst(vst0: &[Vec<[i32; 4]>]) {
+    for line in vst0 {
         for c in line {
-            if *c {
-                print!("  ##");
-            } else {
-                print!("    ");
-            }
+            print!("({:4}, {:4}, {:4}, {:4}) ", c[0], c[1], c[2], c[3]);
         }
         println!();
     }
 }
 
-fn print_vst2(vst2: &[Vec<i32>]) {
+fn print_vst2(vst2: &[Vec<bool>]) {
     for line in vst2 {
         for c in line {
-            if *c == 999999999 {
-                print!("    ");
+            if *c {
+                print!("#");
             } else {
-                print!("{:4}", c);
+                print!(".");
             }
         }
         println!();
@@ -93,18 +88,16 @@ fn bfs(
     map: &mut [Vec<char>],
     s_pos: (i32, i32),
     e_pos: (i32, i32),
-) -> (i32, Vec<Vec<i32>>) {
+) -> (i32, Vec<Vec<[i32; 4]>>) {
     let width = map.len();
     let height = map[0].len();
     let mut vst = vec![vec![[-1; 4]; height]; width];
-    let mut vst2 = vec![vec![999999999; height]; width];
     let mut heap = BinaryHeap::new();
     heap.push(State {
         cost: 0,
         x: s_pos.0,
         y: s_pos.1,
         dir: 0,
-        cost2: 0,
     });
 
     while let Some(State {
@@ -112,20 +105,16 @@ fn bfs(
         x: cx,
         y: cy,
         dir: cd,
-        cost2: cc2,
     }) = heap.pop()
     {
-        if vst[cx as usize][cy as usize][cd as usize] != -1
-            || vst2[cx as usize][cy as usize] < cc2
-        {
+        if vst[cx as usize][cy as usize][cd as usize] != -1 {
             continue;
         }
         vst[cx as usize][cy as usize][cd as usize] = cc;
-        vst2[cx as usize][cy as usize] = cc2;
         // println!("----------------");
         // println!("search: {}, ({}, {}), {}, {}", cc, cx, cy, cd, cc2);
         if cx == e_pos.0 && cy == e_pos.1 {
-            return (cc, vst2);
+            return (cc, vst);
         }
 
         for i in 0..1 {
@@ -136,7 +125,6 @@ fn bfs(
             let nx = cx + dx[cd as usize];
             let ny = cy + dy[cd as usize];
             let nd = cd;
-            let nc2 = cc2 + 1;
 
             if nx < 0 || nx >= width as i32 || ny < 0 || ny >= height as i32 {
                 continue;
@@ -163,7 +151,6 @@ fn bfs(
                 x: nx,
                 y: ny,
                 dir: nd,
-                cost2: nc2,
             });
         }
 
@@ -172,7 +159,6 @@ fn bfs(
             let nx = cx;
             let ny = cy;
             let nd = (cd + i) % 4;
-            let nc2 = cc2;
             if vst[nx as usize][ny as usize][nd as usize] == -1 {
                 // println!("push: {}, ({}, {}), {}, {}", nc, nx, ny, nd, nc2);
                 heap.push(State {
@@ -180,28 +166,30 @@ fn bfs(
                     x: nx,
                     y: ny,
                     dir: nd,
-                    cost2: nc2,
                 });
             }
         }
     }
-    (-1, vst2)
+    (-1, vst)
 }
 
-fn bfs_vst2(vst2: &[Vec<i32>], e_pos: (i32, i32)) -> (i32, Vec<Vec<bool>>) {
-    let width = vst2.len();
-    let height = vst2[0].len();
-    let mut vst3 = vec![vec![false; height]; width];
+fn bfs_vst(vst: &[Vec<[i32; 4]>], e_pos: (i32, i32)) -> (i32, Vec<Vec<bool>>) {
+    let width = vst.len();
+    let height = vst[0].len();
+    let mut vst2 = vec![vec![false; height]; width];
     let mut seats = 0;
     let mut deque = VecDeque::new();
-    let sc = vst2[e_pos.0 as usize][e_pos.1 as usize];
-    deque.push_back((e_pos.0, e_pos.1, sc));
+
+    let cx = e_pos.0;
+    let cy = e_pos.1;
+    let cc = vst[cx as usize][cy as usize];
+    deque.push_back((cx, cy, cc));
 
     while let Some((cx, cy, cc)) = deque.pop_front() {
-        if vst3[cx as usize][cy as usize] {
+        if vst2[cx as usize][cy as usize] {
             continue;
         }
-        vst3[cx as usize][cy as usize] = true;
+        vst2[cx as usize][cy as usize] = true;
         seats += 1;
 
         let dx = [0, 1, 0, -1];
@@ -213,17 +201,21 @@ fn bfs_vst2(vst2: &[Vec<i32>], e_pos: (i32, i32)) -> (i32, Vec<Vec<bool>>) {
             if nx < 0 || nx >= width as i32 || ny < 0 || ny >= height as i32 {
                 continue;
             }
-            if vst3[nx as usize][ny as usize] {
+            if vst2[nx as usize][ny as usize] {
                 continue;
             }
-            let nc = vst2[nx as usize][ny as usize];
-            if nc == -1 || nc >= cc {
-                continue;
+
+            let nc = vst[nx as usize][ny as usize];
+            for j in 0..4 {
+                if nc[j] != -1 && cc[j] - nc[j] == 1 {
+                    deque.push_back((nx, ny, nc));
+                    break;
+                }
             }
-            deque.push_back((nx, ny, nc));
         }
     }
-    (seats, vst3)
+
+    (seats, vst2)
 }
 
 fn main() {
@@ -231,11 +223,13 @@ fn main() {
     print_map(&map);
     println!("s_pos: {:?}", s_pos);
     println!("e_pos: {:?}", e_pos);
-    let (cost, vst2) = bfs(&mut map, s_pos, e_pos);
+
+    let (cost, vst) = bfs(&mut map, s_pos, e_pos);
     println!("cost: {:?}", cost);
     print_map(&map);
-    print_vst2(&vst2);
-    let (seats, vst3) = bfs_vst2(&vst2, e_pos);
+    print_vst(&vst);
+
+    let (seats, vst2) = bfs_vst(&vst, e_pos);
     println!("seats: {:?}", seats);
-    print_vst3(&vst3);
+    print_vst2(&vst2);
 }
