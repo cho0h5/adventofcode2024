f28bf893030f4fe412d96399643ff63e1d0af141
Youngho Cho <dominoyh5@gmail.com>
2024-12-18 14:45:50 +0900

    day18-1: fail

--- day18-1/src/main.rs
+++ day18-1/src/main.rs
@@ -7,7 +7,9 @@ const HEIGHT: usize = 7;
 
 struct Computer {
     memory: Vec<Vec<char>>,
+    vst: Vec<Vec<usize>>,
     coords: VecDeque<(usize, usize)>,
+    deque: VecDeque<(i32, i32, usize)>,
 }
 
 fn input() -> Computer {
@@ -21,8 +23,16 @@ fn input() -> Computer {
         coords.push_back((x, y));
     }
 
-    let mut memory = vec![vec!['.'; HEIGHT]; WIDTH];
-    Computer { memory, coords }
+    let memory = vec![vec!['.'; HEIGHT]; WIDTH];
+    let vst = vec![vec![usize::MAX; HEIGHT]; WIDTH];
+    let deque = VecDeque::from(vec![(0, 0, 1)]);
+
+    Computer {
+        memory,
+        vst,
+        coords,
+        deque,
+    }
 }
 
 impl Computer {
@@ -35,9 +45,48 @@ impl Computer {
         }
     }
 
-    fn step(&mut self) {
+    fn collapse_step(&mut self) {
         if let Some((x, y)) = self.coords.pop_front() {
             self.memory[x][y] = '#';
+        }
+    }
+
+    fn bfs_step(&mut self) -> Option<usize> {
+        let mut next_deque = VecDeque::new();
+
+        while let Some((cx, cy, cc)) = self.deque.pop_front() {
+            if self.vst[cx as usize][cy as usize] <= cc {
+                continue;
+            }
+            self.vst[cx as usize][cy as usize] = cc;
+            self.memory[cx as usize][cy as usize] = 'O';
+
+            let dx = [0, 1, 0, -1];
+            let dy = [1, 0, -1, 0];
+            for i in 0..4 {
+                let nx = cx + dx[i];
+                let ny = cy + dy[i];
+                let nc = cc + 1;
+
+                if nx < 0 || nx >= WIDTH as i32 || ny < 0 || ny >= HEIGHT as i32
+                {
+                    continue;
+                }
+                if self.memory[nx as usize][ny as usize] == '#' {
+                    continue;
+                }
+                if self.vst[nx as usize][ny as usize] <= nc {
+                    continue;
+                }
+                next_deque.push_back((nx, ny, nc));
+            }
+        }
+        self.deque = next_deque;
+
+        if self.vst[WIDTH - 1][HEIGHT - 1] != usize::MAX {
+            Some(self.vst[WIDTH - 1][HEIGHT - 1])
+        } else {
+            None
         }
     }
 }
@@ -49,7 +98,11 @@ fn main() {
 
     for i in 0..computer.coords.len() + 1 {
         println!("------ i: {} ------", i);
-        computer.step();
+        if let Some(cost) = computer.bfs_step() {
+            println!("cost: {}", cost);
+            break;
+        }
+        computer.collapse_step();
         computer.print_memory();
     }
 }
