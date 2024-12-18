ae5f3cbc1049e6efacd046b471e29ccd81f0faf5
Youngho Cho <dominoyh5@gmail.com>
2024-12-18 15:56:33 +0900

    day09-2: impl checksum and print_disk

--- day09-2/src/main.rs
+++ day09-2/src/main.rs
@@ -16,7 +16,10 @@ fn input() -> Vec<FragState> {
     for (i, c) in line.chars().enumerate() {
         let num = c as usize - '0' as usize;
         if i % 2 == 0 {
-            disk.push(FragState::File { size: num, id: i as usize / 2 });
+            disk.push(FragState::File {
+                size: num,
+                id: i as usize / 2,
+            });
         } else {
             disk.push(FragState::Free { size: num });
         }
@@ -25,22 +28,71 @@ fn input() -> Vec<FragState> {
     disk
 }
 
-fn compacting(disk: &mut Vec<FragState>) {
-}
+fn compacting(disk: &mut Vec<FragState>) {}
 
 fn checksum(disk: &Vec<FragState>) -> usize {
     let mut sum = 0;
 
+    let mut temp = Vec::new();
+    for frag in disk {
+        match frag {
+            FragState::File { size, id } => {
+                for _ in 0..*size {
+                    temp.push(*id as i32);
+                }
+            }
+            FragState::Free { size } => {
+                for _ in 0..*size {
+                    temp.push(-1);
+                }
+            }
+        }
+    }
+
+    for (i, num) in temp.iter().enumerate() {
+        if *num != -1 {
+            sum += i * *num as usize;
+        }
+    }
+
     sum
+}
+
+fn print_disk(disk: &[FragState]) {
+    let mut temp = Vec::new();
+    for frag in disk {
+        match frag {
+            FragState::File { size, id } => {
+                for _ in 0..*size {
+                    temp.push(*id as i32);
+                }
+            }
+            FragState::Free { size } => {
+                for _ in 0..*size {
+                    temp.push(-1);
+                }
+            }
+        }
+    }
+
+    print!("disk: [");
+    for num in temp {
+        if num == -1 {
+            print!("{} ", '.');
+        } else {
+            print!("{} ", num);
+        }
+    }
+    println!("]");
 }
 
 fn main() {
     let mut disk = input();
-    println!("before: {:?}", disk);
+    print_disk(&disk);
 
-//    compacting(&mut disk);
-//    println!("after: {:?}", disk);
-//
-//    let sum = checksum(&disk);
-//    println!("checksum: {:?}", sum);
+    compacting(&mut disk);
+    print_disk(&disk);
+
+    let sum = checksum(&disk);
+    println!("checksum: {:?}", sum);
 }
