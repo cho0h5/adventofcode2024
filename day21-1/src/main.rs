af1236f9d0689793c6daeebb0bd7af4817d94f0e
Youngho Cho <dominoyh5@gmail.com>
2024-12-21 15:56:27 +0900

    day21-1: add directional map

--- day21-1/src/main.rs
+++ day21-1/src/main.rs
@@ -50,6 +50,45 @@ fn generate_numeric_map() -> NumericMap {
     map
 }
 
+//   ^ A
+// < v >
+type DirectionalMap = HashMap<(char, char), &'static str>;
+fn generate_directional_map() -> DirectionalMap {
+    let mut map = HashMap::new();
+
+    map.insert(('A', 'A'), "A");
+    map.insert(('A', '^'), "<A");
+    map.insert(('A', '<'), "v<<A");
+    map.insert(('A', 'v'), "<vA"); // v< <v
+    map.insert(('A', '>'), "vA");
+
+    map.insert(('^', 'A'), ">A");
+    map.insert(('^', '^'), "A");
+    map.insert(('^', '<'), "v<A");
+    map.insert(('^', 'v'), "vA");
+    map.insert(('^', '>'), ">vA"); // >v v>
+
+    map.insert(('<', 'A'), ">>^A");
+    map.insert(('<', '^'), ">^A");
+    map.insert(('<', '<'), "A");
+    map.insert(('<', 'v'), ">A");
+    map.insert(('<', '>'), ">>A");
+
+    map.insert(('v', 'A'), ">^A"); // >^ ^>
+    map.insert(('v', '^'), "^A");
+    map.insert(('v', '<'), "<A");
+    map.insert(('v', 'v'), "A");
+    map.insert(('v', '>'), ">A");
+
+    map.insert(('>', 'A'), "^A");
+    map.insert(('>', '^'), "<^A"); // <^ ^>
+    map.insert(('>', '<'), "<<A");
+    map.insert(('>', 'v'), "<A");
+    map.insert(('>', '>'), "A");
+
+    map
+}
+
 fn resolve_numeric_keypad(numeric_map: &NumericMap, code: &str) -> Vec<String> {
     let mut result = String::new();
 
@@ -64,21 +103,37 @@ fn resolve_numeric_keypad(numeric_map: &NumericMap, code: &str) -> Vec<String> {
     vec![result]
 }
 
-fn resolve_directional_keypad(code: &str) -> String {
-    String::from("bar")
+fn resolve_directional_keypad(
+    directional_map: &DirectionalMap,
+    code: &str,
+) -> String {
+    let mut result = String::new();
+
+    result.push_str(directional_map[&('A', code.as_bytes()[0] as char)]);
+    for i in 0..code.len() - 1 {
+        result.push_str(
+            directional_map
+                [&(code.as_bytes()[i] as char, code.as_bytes()[i + 1] as char)],
+        );
+    }
+
+    result
 }
 
 fn main() {
     let codes = input();
     let numeric_map = generate_numeric_map();
+    let directional_map = generate_directional_map();
 
     for code in &codes {
         let resolved1s = resolve_numeric_keypad(&numeric_map, code);
         println!("----------------");
         println!("code: {}", code);
         for resolved1 in resolved1s {
-            let resolved2 = resolve_directional_keypad(&resolved1);
-            let resolved3 = resolve_directional_keypad(&resolved2);
+            let resolved2 =
+                resolve_directional_keypad(&directional_map, &resolved1);
+            let resolved3 =
+                resolve_directional_keypad(&directional_map, &resolved2);
             println!("resolved1: {}", resolved1);
             println!("resolved2: {}", resolved2);
             println!("resolved3: {}", resolved3);
