13fc881420673080f0c4225a3a28b6f7b45a47db
Youngho Cho <dominoyh5@gmail.com>
2024-12-23 16:32:44 +0900

    day23-2: input

--- day23-2/src/main.rs
+++ day23-2/src/main.rs
@@ -3,22 +3,22 @@ use std::collections::HashSet;
 use std::env;
 use std::fs::read_to_string;
 
-type Graph = HashMap<String, HashMap<String, bool>>;
+type Graph = HashMap<String, HashSet<String>>;
 
 fn input() -> Graph {
-    let mut map: HashMap<String, HashMap<String, bool>> = HashMap::new();
+    let mut map: Graph = HashMap::new();
 
     let filename = env::args().nth(1).unwrap();
     for line in read_to_string(filename).unwrap().lines() {
         let (part1, part2) = line.split_once('-').unwrap();
 
         let inner_map =
-            map.entry(part1.to_owned()).or_insert_with(HashMap::new);
-        inner_map.insert(part2.to_owned(), true);
+            map.entry(part1.to_owned()).or_insert_with(HashSet::new);
+        inner_map.insert(part2.to_owned());
 
         let inner_map =
-            map.entry(part2.to_owned()).or_insert_with(HashMap::new);
-        inner_map.insert(part1.to_owned(), true);
+            map.entry(part2.to_owned()).or_insert_with(HashSet::new);
+        inner_map.insert(part1.to_owned());
     }
 
     map
@@ -31,10 +31,10 @@ fn bf(graph: &Graph, ts: &HashSet<String>) -> HashSet<Vec<String>> {
 
         println!("{}, {:?}", t, t_map);
 
-        for u in t_map.keys() {
+        for u in t_map {
             if let Some(u_map) = graph.get(u) {
-                for v in u_map.keys() {
-                    if t_map.contains_key(v) {
+                for v in u_map {
+                    if t_map.contains(v) {
                         let mut key = vec![t.clone(), u.clone(), v.clone()];
                         key.sort();
                         inter_connected.insert(key);
