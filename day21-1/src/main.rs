39588eb27bdf75d5b170f778822a410f5c809465
Youngho Cho <dominoyh5@gmail.com>
2024-12-23 13:59:03 +0900

    day21-1: generate numpad path

--- day21-1/src/main.rs
+++ day21-1/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::env;
 use std::fs::read_to_string;
 
@@ -13,7 +12,69 @@ fn input() -> Vec<String> {
     codes
 }
 
+fn find_numkey_pos(ch: char) -> Option<(i32, i32)> {
+    let numkey = [
+        vec!['7', '8', '9'],
+        vec!['4', '5', '6'],
+        vec!['1', '2', '3'],
+        vec![' ', '0', 'A'],
+    ];
+
+    for (i, line) in numkey.iter().enumerate() {
+        for (j, c) in line.iter().enumerate() {
+            if ch == *c {
+                return Some((i as i32, j as i32));
+            }
+        }
+    }
+    None
+}
+
+fn gen_numkey_path(from: char, to: char) -> Vec<String> {
+    let from_pos = find_numkey_pos(from).unwrap();
+    let to_pos = find_numkey_pos(to).unwrap();
+
+    let dx = to_pos.0 - from_pos.0;
+    let dy = to_pos.1 - from_pos.1;
+
+    let vertical = if dx < 0 {
+        "^".repeat(-dx as usize)
+    } else {
+        "v".repeat(dx as usize)
+    };
+
+    let horizontal = if dy < 0 {
+        "<".repeat(-dy as usize)
+    } else {
+        ">".repeat(dy as usize)
+    };
+
+    if vertical.is_empty() {
+        vec![horizontal]
+    } else if horizontal.is_empty() {
+        vec![vertical]
+    } else if from_pos.0 == 3 && to_pos.1 == 0 {
+        vec![vertical + &horizontal]
+    } else if from_pos.1 == 0 && to_pos.0 == 3 {
+        vec![horizontal + &vertical]
+    } else {
+        vec![vertical.clone() + &horizontal, horizontal + &vertical]
+    }
+}
+
+fn cal_numkey(code: &str) {
+    let code = String::from("A") + code;
+    let code = code.as_bytes();
+
+    for i in 0..code.len() - 1 {
+        println!("{} {}", code[i] as char, code[i + 1] as char);
+        let paths = gen_numkey_path(code[i] as char, code[i + 1] as char);
+        println!("{:?}", paths);
+    }
+}
+
 fn main() {
     let codes = input();
     println!("codes: {:?}", codes);
+    cal_numkey(&codes[0]);
 }
