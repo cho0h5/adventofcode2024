7dc271ba370b039948cc61a89a1309b97669b4a9
Youngho Cho <dominoyh5@gmail.com>
2024-12-24 13:49:49 +0900

    day23-2: graph print

--- day23-2/src/main.rs
+++ day23-2/src/main.rs
@@ -3,10 +3,11 @@ use std::collections::HashSet;
 use std::env;
 use std::fs::read_to_string;
 
-type Graph = HashMap<String, HashSet<String>>;
+#[derive(Debug)]
+struct Graph(HashMap<String, HashSet<String>>);
 
 fn input() -> Graph {
-    let mut map: Graph = HashMap::new();
+    let mut map = HashMap::new();
 
     let filename = env::args().nth(1).unwrap();
     for line in read_to_string(filename).unwrap().lines() {
@@ -21,33 +22,18 @@ fn input() -> Graph {
         inner_map.insert(part1.to_owned());
     }
 
-    map
+    Graph(map)
 }
 
-fn bf(graph: &Graph, ts: &HashSet<String>) -> HashSet<Vec<String>> {
-    let mut inter_connected = HashSet::new();
-    for t in ts {
-        let t_map = &graph[t];
-
-        println!("{}, {:?}", t, t_map);
-
-        for u in t_map {
-            if let Some(u_map) = graph.get(u) {
-                for v in u_map {
-                    if t_map.contains(v) {
-                        let mut key = vec![t.clone(), u.clone(), v.clone()];
-                        key.sort();
-                        inter_connected.insert(key);
-                    }
-                }
-            }
+impl Graph {
+    fn print(&self) {
+        for (k, v) in &self.0 {
+            println!("{}: {:?}", k, v);
         }
     }
-
-    inter_connected
 }
 
 fn main() {
     let graph = input();
-    println!("{:?}", graph);
+    graph.print();
 }
