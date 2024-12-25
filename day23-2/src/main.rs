d2ad70a56fd773d28b48818dd00d47fe7b0c5d46
Youngho Cho <dominoyh5@gmail.com>
2024-12-25 12:41:05 +0900

    day23-2

--- day23-2/src/main.rs
+++ day23-2/src/main.rs
@@ -33,7 +33,49 @@ impl Graph {
     }
 }
 
+fn bron_kerbosch(
+    graph: &Graph,
+    r: &mut Vec<String>,
+    p: &mut HashSet<String>,
+    x: &mut HashSet<String>,
+) -> Vec<Vec<String>> {
+    if p.is_empty() && x.is_empty() {
+        return vec![r.clone()];
+    }
+
+    let mut cliques = Vec::new();
+    for v in p.clone() {
+        r.push(v.clone());
+        let mut p_intersection =
+            p.intersection(&graph.0[&v]).cloned().collect();
+        let mut x_intersection =
+            x.intersection(&graph.0[&v]).cloned().collect();
+
+        cliques.extend(bron_kerbosch(
+            graph,
+            r,
+            &mut p_intersection,
+            &mut x_intersection,
+        ));
+
+        r.pop();
+        p.remove(&v);
+        x.insert(v);
+    }
+    cliques
+}
+
 fn main() {
     let graph = input();
     graph.print();
+
+    let mut r = Vec::new();
+    let mut p = graph.0.keys().cloned().collect::<HashSet<_>>();
+    let mut x = HashSet::new();
+    let cliques = bron_kerbosch(&graph, &mut r, &mut p, &mut x);
+
+    let mut maximum_clique =
+        cliques.into_iter().max_by_key(|c| c.len()).unwrap();
+    maximum_clique.sort();
+    println!("maximum clique: {}", maximum_clique.join(","));
 }
